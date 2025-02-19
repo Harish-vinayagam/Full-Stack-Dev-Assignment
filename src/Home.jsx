95d93dfd582d03bca59b408775f868f7f0ba4368
Z4_ REDSOUL <vinhari2212@gmail.com>
2025-02-19 10:41:52 +0530

    Initial commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Harish Vinayagam</h1>
       <p>Contact: Harish.v.s74@kalvium.community</p>
-      <p>Phone: 8939560448</p>
+      <p>Phone: Skibidi</p>
     </div>
   );
 };
