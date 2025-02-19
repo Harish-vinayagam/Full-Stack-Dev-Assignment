b32d3edfaa85aae363246e63024546fd3f6062bb
Z4_ REDSOUL <vinhari2212@gmail.com>
2025-02-19 10:35:05 +0530

    Initial commit

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Harish Vinayagam</h1>
+      <p>Contact: Harish.v.s74@kalvium.community</p>
+      <p>Phone: 8939560448</p>
+    </div>
+  );
+};
+
+export default Home;
